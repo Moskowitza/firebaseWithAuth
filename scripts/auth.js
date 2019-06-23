1bc927635b23a2b43380aa68b6cdfe7237ac0d95
moskowitza <moskowitzaaron@mac.com>
2019-06-23 13:19:16 -0400

    adding new climbs

--- scripts/auth.js
+++ scripts/auth.js
@@ -2,23 +2,52 @@
 const signupForm = document.querySelector('#signup-form');
 const logout = document.querySelector('#logout');
 const loginForm = document.querySelector('#login-form');
+const createForm = document.querySelector('#create-form');
 
 // connect to database
-db.collection('climbs')
-  .get()
-  .then(snapshot => {
-    console.log(snapshot.docs);
-    setUpClimbs(snapshot.docs);
-  });
 
 auth.onAuthStateChanged(user => {
   if (user) {
     console.log(`logged in user: ${JSON.stringify(user)}`);
+    // if there is a user, get the climbs
+    setupUI(user);
+    db.collection('climbs')
+      .get()
+      .then(snapshot => {
+        console.log(snapshot.docs);
+        setUpClimbs(snapshot.docs);
+      });
   } else {
-    console.log('no user');
+    setUpClimbs([]);
+    setupUI(user);
   }
 });
 
+// create form
+createForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const routeName = createForm.routeName.value;
+  const grade = createForm.grade.value;
+  console.log(grade + routeName);
+  db.collection('climbs')
+    .add({
+      routeName,
+      grade,
+    })
+    .then(res => {
+      console.log(res);
+      const modal = $('#modal-create');
+      modal.modal('hide');
+      createForm.reset();
+    })
+    .catch(function(error) {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ...
+      console.log(`${errorCode}  ${errorMessage}`);
+    });
+});
 // SIGN UP NEW USER
 signupForm.addEventListener('submit', e => {
   e.preventDefault();
@@ -43,9 +72,9 @@ signupForm.addEventListener('submit', e => {
 // LOG OUT
 logout.addEventListener('click', e => {
   e.preventDefault();
+  console.log(`signed out`);
   auth.signOut().then(res => {
     // console.log(res);
-    console.log(`signed out`);
   });
 });
 // Login
@@ -66,6 +95,6 @@ loginForm.addEventListener('submit', e => {
       const errorCode = error.code;
       const errorMessage = error.message;
       // ...
-      console.log(`${errCode}  ${errorMessage}`);
+      console.log(`${errorCode}  ${errorMessage}`);
     });
 });
--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,56 @@
 const climbAccordion = document.querySelector('#accordion');
-const setUpClimbs = data => {
-  let html = '';
-  data.forEach((doc, i) => {
-    const climb = doc.data();
-    console.log(`${climb}, ${i}`);
-    const card = `
-                <div class="card">
-                <div class="card-header" id="heading${i}">
-                  <h5 class="mb-0">
-                    <button
-                      class="btn btn-link"
-                      data-toggle="collapse"
-                      data-target="#collapse${i}"
-                      aria-expanded="true"
-                      aria-controls="collapse${i}"
-                    >
-                      ${climb.routeName}
-                    </button>
-                  </h5>
-                </div>
+const loggedOutLinks = document.querySelectorAll('.logged-out');
+const loggedInLinks = document.querySelectorAll('.logged-in');
 
-                <div
-                  id="collapse${i}"
-                  class="collapse show"
-                  aria-labelledby="heading${i}"
-                  data-parent="#accordion"
-                >
-                  <div class="card-body">
-                    <p>${climb.routeName}</p>
-                    <p>${climb.grade}</p>
-                  </div>
-                </div>
-                </div>`;
-    html += card;
-  });
-  climbAccordion.innerHTML = html;
+const setupUI = user => {
+  if (user) {
+    loggedInLinks.forEach(link => (link.style.display = 'block'));
+    loggedOutLinks.forEach(link => (link.style.display = 'none'));
+  } else {
+    loggedInLinks.forEach(link => (link.style.display = 'none'));
+    loggedOutLinks.forEach(link => (link.style.display = 'block'));
+  }
+};
+
+const setUpClimbs = data => {
+  if (data.length) {
+    let html = '';
+    data.forEach((doc, i) => {
+      const climb = doc.data();
+      console.log(`${climb}, ${i}`);
+      const card = `
+            <div class="card">
+            <div class="card-header" id="heading${i}">
+            <h5 class="mb-0">
+            <button
+            class="btn btn-link"
+            data-toggle="collapse"
+            data-target="#collapse${i}"
+            aria-expanded="true"
+            aria-controls="collapse${i}"
+            >
+            ${climb.routeName}
+            </button>
+            </h5>
+            </div>
+            
+            <div
+            id="collapse${i}"
+            class="collapse show"
+            aria-labelledby="heading${i}"
+            data-parent="#accordion"
+            >
+            <div class="card-body">
+            <p>${climb.routeName}</p>
+            <p>${climb.grade}</p>
+            </div>
+            </div>
+            </div>`;
+      html += card;
+    });
+    climbAccordion.innerHTML = html;
+  } else {
+    const html = `<h5>login to view routes</h5>`;
+    climbAccordion.innerHTML = html;
+  }
 };
